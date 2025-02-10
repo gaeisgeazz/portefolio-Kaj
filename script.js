67a9b34f72f5ecea900448e213724e7f2bb5f0d6
gaeisgeazz <gaeisgeazz@gmail.com>
2025-02-10 09:20:20 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function valider(){
-  nom=document.getElementById('nom').value;
-  prenom=document.getElementById('prenom').value;
-  if((!nom)&& (!prenom)){
-    alert("veuillez remplir ces champs");
-  }else{
-    alert("vous avez valider l'inscription");
-  }
-}
